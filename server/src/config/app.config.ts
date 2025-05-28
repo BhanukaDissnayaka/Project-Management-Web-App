9f6d9a60537a78707718bf84814eb3ad955737c9
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-05-28 21:21:38 +0530

    refactor: DB connection and add config files

---
+++ server/src/config/app.config.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+type AppConfig = {
+  NODE_ENV: "development" | "production" | "test";
+  PORT: number;
+  BASE_PATH: string;
+  MONGO_URI: string;
+  JWT_SECRET: string;
+  JWT_EXPIRES_IN: string;
+};
+
+const appConfig = (): AppConfig => ({
+  NODE_ENV: (process.env.NODE_ENV as AppConfig["NODE_ENV"]) || "development",
+  PORT: parseInt(process.env.PORT || "5000", 10),
+  BASE_PATH: "/api",
+  MONGO_URI: process.env.MONGO_URI || "",
+  JWT_SECRET: process.env.JWT_SECRET || "supersecret",
+  JWT_EXPIRES_IN: "7d",
+});
+
+export const config = appConfig();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import authRouter from "./routes/auth.route";
 import { connectDB } from "./config/db";
+import { config } from "./config/app.config";
 
-dotenv.config();
 const app = express();
+const BASE_PATH = config.BASE_PATH;
 
-const PORT: number = Number(process.env.PORT) || 5000;
+app.use(`${BASE_PATH}/auth/`, authRouter);
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(config.PORT, () => {
+    console.log(`Server running on http://localhost:${config.PORT}`);
   });
 };
 
