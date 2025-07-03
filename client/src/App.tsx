a440cfc1474871920e14b10eb6bf98b9a8ed4b0c
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-07-03 19:55:56 +0530

    feat(frontend): create toast handler function and add Toaster to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Toaster position="top-center" reverseOrder={false} />
+    </>
+  );
 }
 
 export default App;
