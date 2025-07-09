0e9dab6567fbdfeb4fac7d4af289fb1cd6f8c218
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-07-09 17:32:04 +0530

    feat(frontend): add router path for signin

--- client/src/routes/common/routes.tsx
+++ client/src/routes/common/routes.tsx
@@ -1,6 +1,8 @@
 import { SignUp } from "@/pages/auth/sign-up";
 import { AUTH_ROUTES } from "./routePaths";
+import { SignIn } from "@/pages/auth/sign-in";
 
 export const authenticationRoutePaths = [
   { path: AUTH_ROUTES.SIGN_UP, element: <SignUp /> },
+  { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
 ];
