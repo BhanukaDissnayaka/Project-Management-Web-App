4157b50114ff5d4a0d40dca8db33a20ae88eb32b
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-07-16 11:57:13 +0530

    fix(frontend): add loading state to AuthRoute to prevent flicker and remove isAuthRoute check

--- client/src/routes/auth.route.tsx
+++ client/src/routes/auth.route.tsx
@@ -1,15 +1,11 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { isAuthRoute } from "./common/routePaths";
+import { Navigate, Outlet } from "react-router-dom";
 import { useGetCurrentUserQuery } from "@/features/authentication/api/auth.api";
 
 const AuthRoute = () => {
-  const location = useLocation();
-  const _isAuthRoute = isAuthRoute(location.pathname);
-
   const { data: authData, isLoading } = useGetCurrentUserQuery();
   const user = authData?.user;
 
-  if (!_isAuthRoute && isLoading) return <h1>Loading</h1>;
+  if (isLoading) return <h1>Login Page Loading</h1>;
 
   if (!user) return <Outlet />;
 
