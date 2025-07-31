d0980cd5f8dc4fdc9ee173c42620e3803a4215d6
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-07-31 14:53:30 +0530

    feat(frontend): feat: add scaffold WorkspaceDashboard component

---
+++ client/src/pages/workspace/WorkspaceDashboard.tsx
@@ -0,0 +1,6 @@
+const WorkspaceDashboard = () => {
+  // todo - Should implement other workspace dash board features
+  return <div>Workspace Dash Board</div>;
+};
+
+export default WorkspaceDashboard;
