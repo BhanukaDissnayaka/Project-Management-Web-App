3e72c8149714fb7dd8c8bc89d634520d1a6161dc
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-07-31 15:00:44 +0530

    feat(frontend):  add protected route for WorkspaceDashboard under AppLayout

--- client/src/routes/common/routes.tsx
+++ client/src/routes/common/routes.tsx
@@ -1,8 +1,13 @@
 import { SignUp } from "@/pages/auth/sign-up";
-import { AUTH_ROUTES } from "./routePaths";
+import { AUTH_ROUTES, PROTECTED_ROUTES } from "./routePaths";
 import { SignIn } from "@/pages/auth/sign-in";
+import WorkspaceDashboard from "@/pages/workspace/WorkspaceDashboard";
 
 export const authenticationRoutePaths = [
   { path: AUTH_ROUTES.SIGN_UP, element: <SignUp /> },
   { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
+];
+export const protectedRoutePaths = [
+  { path: PROTECTED_ROUTES.WORKSPACE, element: <WorkspaceDashboard /> },
+  ,
 ];
