0800d487433d28e86acec2597517791f65b6d1c1
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-08-07 01:15:02 +0530

    feat(frontend): install nuqs and implement useCreateWorkspaceDialog for handle create workspace dialog

---
+++ client/src/hooks/useCreateWorkspaceDisalog.tsx
@@ -0,0 +1,16 @@
+import { parseAsBoolean, useQueryState } from "nuqs";
+
+const useCreateWorkspaceDialog = () => {
+  const [open, setOpen] = useQueryState(
+    "new-workspace",
+    parseAsBoolean.withDefault(false)
+  );
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+  return {
+    open,
+    onOpen,
+    onClose,
+  };
+};
+export default useCreateWorkspaceDialog;
