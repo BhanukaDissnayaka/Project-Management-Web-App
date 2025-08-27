9eb1e9234800eaad30ac61f409bc86a5773448ce
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-08-27 17:35:20 +0530

    feat(backend): implement addMemberToWorkspaceService function

--- server/src/services/workspace.service.ts
+++ server/src/services/workspace.service.ts
@@ -3,7 +3,11 @@ import UserModel from "../models/user.model";
 import WorkspaceMemberModel from "../models/workspace-member.model";
 import WorkspaceRoleModel from "../models/workspace-role-permission.model";
 import WorkspaceModel from "../models/workspace.model";
-import { NotFoundException, UnauthorizedException } from "../utils/appError";
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from "../utils/appError";
 import mongoose from "mongoose";
 
 export const createWorkspaceService = async (
@@ -46,6 +50,50 @@ export const createWorkspaceService = async (
   return {
     workspace,
   };
+};
+export const addMemberToWorkspaceService = async (
+  userId: string,
+  workspaceId: string
+) => {
+  // check workspace exit
+  const workspace = await WorkspaceModel.findById(workspaceId);
+  if (!workspace) {
+    throw new NotFoundException("workspace not found");
+  }
+
+  // check user is exist
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    throw new NotFoundException("user not found");
+  }
+
+  // Check if user is already a member
+  const existingMember = await WorkspaceMemberModel.findOne({
+    userId,
+    workspaceId: workspace._id,
+  });
+  if (existingMember) {
+    throw new BadRequestException(
+      "This user is already a member of this workspace"
+    );
+  }
+
+  const role = await WorkspaceRoleModel.findOne({
+    name: WorkspaceRoles.MEMBER,
+  });
+  if (!role) {
+    throw new NotFoundException("Role not found");
+  }
+
+  // Add user to workspace as a member
+  const newMember = new WorkspaceMemberModel({
+    userId,
+    workspaceId: workspace._id,
+    role: role._id,
+  });
+  await newMember.save();
+
+  return { workspaceId: workspace._id, role: role.name };
 };
 
 export const getWorkspaceByIdService = async (
