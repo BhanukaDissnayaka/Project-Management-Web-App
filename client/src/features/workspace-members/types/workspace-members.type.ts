4269dd38a4e06858b242ad88af3931a08b23e513
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-09-04 19:11:52 +0530

    feat(fronten): add some type

--- client/src/features/workspace-members/types/workspace-members.type.ts
+++ client/src/features/workspace-members/types/workspace-members.type.ts
@@ -25,3 +25,43 @@ export type AddUserToWorkspaceResponseType = {
   workspaceId: string;
   userId: string;
 };
+export type getWorkspaceMembersType = {
+  searchValue: string;
+  workspaceId: string;
+  page: number;
+  limit: number;
+};
+
+export type getWorkspaceMembersResponseType = {
+  message: string;
+  members: {
+    _id: string;
+    userId: string;
+    workspaceId: string;
+    role: {
+      _id: string;
+      name: string;
+    };
+    joinedAt: string;
+    createdAt: string;
+    updatedAt: string;
+    user: {
+      _id: string;
+      name: string;
+      email: string;
+      profilePicture: string | null;
+    };
+  }[];
+  pagination: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+  roles: RoleType[];
+};
+
+export type RoleType = {
+  _id: string;
+  name: string;
+};
