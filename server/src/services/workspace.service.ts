4aa96e4c1b4a7bb7a2d09a88fb782455766be058
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-10-15 18:27:33 +0530

    feat(backend): add updateWorkspaceByIdService function and zod schema validation

--- server/src/services/workspace.service.ts
+++ server/src/services/workspace.service.ts
@@ -257,3 +257,19 @@ export const removeWorkspaceMemberService = async (
     session.endSession();
   }
 };
+
+export const updateWorkspaceByIdService = async (
+  workspaceId: string,
+  name: string,
+  description?: string
+) => {
+  const workspace = await WorkspaceModel.findById(workspaceId);
+  if (!workspace) {
+    throw new NotFoundException("Workspace not found");
+  }
+  workspace.name = name || workspace.name;
+  workspace.description = description || workspace.description;
+  await workspace.save();
+
+  return { workspace };
+};
