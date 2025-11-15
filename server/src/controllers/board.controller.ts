04ddc510ad6a2efd8b719f8fb8d9b4121b63dae0
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-11-15 23:48:56 +0530

    feat(backend): implement getBoardByIdAndWorkspaceController   with board permission check

--- server/src/controllers/board.controller.ts
+++ server/src/controllers/board.controller.ts
@@ -7,9 +7,13 @@ import { checkWorkspacePermission } from "../utils/check-workspace-permission";
 import { WorkspacePermissions } from "../enums/workspace-role.enum";
 import {
   createBoardService,
+  getBoardByIdAndWorkspaceService,
   getBoardsInWorkspaceService,
 } from "../services/board.service";
 import { HTTPSTATUS } from "../config/http.config";
+import { getMemberInBoardService } from "../services/board-member.service";
+import { BoardPermissions } from "../enums/board-role.enum";
+import { checkBoardPermission } from "../utils/check-board-permission";
 
 export const createBoardController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -54,6 +58,26 @@ export const getBoardsInWorkspaceController = asyncHandler(
         totalPages,
         skip,
       },
+    });
+  }
+);
+
+export const getBoardByIdAndWorkspaceController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const workspaceId = workspaceIdSchema.parse(req.params.workspaceId);
+    const boardId = req.params.boardId;
+    const userId = req.user?._id;
+    const { member } = await getMemberInBoardService(userId, boardId);
+    checkBoardPermission(member.role, [BoardPermissions.ADD_BOARD_MEMBER]);
+    const { board, currentBoardMember } = await getBoardByIdAndWorkspaceService(
+      workspaceId,
+      boardId,
+      userId
+    );
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Board fetched successfully",
+      board,
+      currentBoardMember,
     });
   }
 );
