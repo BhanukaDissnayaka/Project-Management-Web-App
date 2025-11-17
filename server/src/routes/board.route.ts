91649c0bf6121bc92b384eea365039d36c2b5124
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-11-17 14:03:56 +0530

    feat(backend): add backend route for updaing board

--- server/src/routes/board.route.ts
+++ server/src/routes/board.route.ts
@@ -3,12 +3,15 @@ import {
   createBoardController,
   getBoardByIdAndWorkspaceController,
   getBoardsInWorkspaceController,
+  updateBoardController,
 } from "../controllers/board.controller";
 
 const boardRoutes = Router({ mergeParams: true });
 
 boardRoutes.post("/create", createBoardController);
+boardRoutes.post("/:boardId/update", updateBoardController);
 boardRoutes.get("/all", getBoardsInWorkspaceController);
 boardRoutes.get("/:boardId", getBoardByIdAndWorkspaceController);
+boardRoutes.post("/:boardId/update", updateBoardController);
 
 export default boardRoutes;
