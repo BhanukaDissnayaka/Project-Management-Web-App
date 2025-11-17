0ce609e3ae7020edc0f41ac25530af911845644f
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-11-17 11:10:18 +0530

    feat(frontend): replace board nav name with real data

--- client/src/features/board/components/BoardNav.tsx
+++ client/src/features/board/components/BoardNav.tsx
@@ -17,6 +17,7 @@ import {
 import { Link } from "react-router-dom";
 import useWorkspaceId from "@/hooks/useWorkspaceId";
 import useBoardId from "@/hooks/useBoardId";
+import { useGetBoardByIdAndWorkspaceQuery } from "../api/board.api";
 type BoardMenuType = {
   title: string;
   url?: string;
@@ -48,10 +49,13 @@ function BoardNav() {
       icon: Tags,
     },
   ];
+
+  const { data } = useGetBoardByIdAndWorkspaceQuery({ workspaceId, boardId });
+
   return (
     <header className="fixed top-0 left-0 h-[var(--board-nav-height)]  md:left-[var(--sidebar-width)] right-0   border-b shadow-b-sm flex items-center z-50 bg-sidebar-primary-foreground">
       <div className="flex items-center justify-between w-full px-4">
-        <div className="font-semibold text-xl pl-5">John keels</div>
+        <div className="font-semibold text-xl pl-5">{data?.board.name}</div>
         <DropdownMenu>
           <DropdownMenuTrigger>
             <MoreHorizontalIcon className="text-gray-text" />
