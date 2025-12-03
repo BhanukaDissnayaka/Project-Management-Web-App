9be8a6a851d2af94eb08796b8ccea79e152b14b5
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-12-03 21:52:39 +0530

    feat(frontend): add AddBoardMembersDialog to BoardMembersPage with a controll btn

--- client/src/pages/board-members/BoardMembersPage.tsx
+++ client/src/pages/board-members/BoardMembersPage.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import AddBoardMembersDialog from "@/features/board-members/components/AddBoardMembersDialog";
+import useAddBoardMembersDialog from "@/features/board-members/hooks/useAddBoardMembersDialog";
+
 function BoardMembersPage() {
-  return <div>BoardMembersPage</div>;
+  const { onOpen } = useAddBoardMembersDialog();
+  return (
+    <div>
+      <div className="flex justify-between">
+        <p className="text-xl font-semibold">Board Members</p>
+        <Button onClick={onOpen}>Add Members</Button>
+      </div>
+      <AddBoardMembersDialog></AddBoardMembersDialog>
+    </div>
+  );
 }
 
 export default BoardMembersPage;
