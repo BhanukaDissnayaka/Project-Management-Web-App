7412aa14a3a920c7847f30869539dc93d6d2e358
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-12-04 21:05:01 +0530

    feat(backend): add backend route for getBoardMembersController

--- server/src/routes/board.route.ts
+++ server/src/routes/board.route.ts
@@ -4,6 +4,7 @@ import {
   createBoardController,
   getAvailableMembersController,
   getBoardByIdAndWorkspaceController,
+  getBoardMembersController,
   getBoardsInWorkspaceController,
   updateBoardController,
 } from "../controllers/board.controller";
@@ -16,6 +17,7 @@ boardRoutes.post("/:boardId/members", addMemberToBoardController);
 boardRoutes.get("/all", getBoardsInWorkspaceController);
 boardRoutes.get("/:boardId", getBoardByIdAndWorkspaceController);
 boardRoutes.get("/:boardId/available-members", getAvailableMembersController);
+boardRoutes.get("/:boardId/members", getBoardMembersController);
 
 export default boardRoutes;
 addMemberToBoardController;
